3664acfbeae24a687b83197a357d37561da8a5df
Ryan Winograd <ryan@thewinograds.com>
2014-12-16 22:08:23 -0600

    ex6

---
+++ ex6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  int distance = 100;
+  float power = 2.345f;
+  double super_power = 56789.4532;
+  char initial = 'L';
+  char first_name[] = "Ryan";
+  char last_name[] = "Winograd";
+  char empty_string[] = "";
+
+  printf("You are %d miles away.\n", distance);
+  printf("You have %f levels of power.\n", power);
+  printf("You have %f awesome super powers.\n", super_power);
+  printf("You have an initial %c.\n", initial);
+  printf("I have a first name %s.\n", first_name);
+  printf("I have a last name %s.\n", last_name);
+  printf("My whole name is %s %c. %s.\n", first_name, initial, last_name);
+  printf("Here's an empty string: %s.\n", empty_string);
+
+  return 0;
+}
